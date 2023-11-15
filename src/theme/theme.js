6111dba282a2ef1f9932d971e7ed1f9285f00d30
Miles Davis <foldes@mail.com>
2023-11-15 23:01:32 +0100

    main structure

---
+++ src/theme/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        }, 
+        background: {
+            default: '#221C1F'
+        }
+    }
+});
+
+export default theme;
