4cb02b6400552ae7d12ceda41428565fe3ff1efd
Miles Davis <foldes@mail.com>
2023-11-18 23:26:40 +0100

    username update

--- src/components/elements/UserName/UserName.js
+++ src/components/elements/UserName/UserName.js
@@ -1,15 +1,38 @@
 import { useContext, useState } from "react";
-import { Typography } from "@mui/material";
+import { Typography, Box, Button, TextField } from "@mui/material";
 import Context from "../../context/userContext";
 
 
 const UserName = () => {
-    const { isAdmin } = useContext(Context);
+    const { isAdmin, userData, updateUsername } = useContext(Context);
+    const [username, setUsername] = useState('');
+
+    if (isAdmin) {
+        return <Typography> Udvozlunk, Uzemeleteto. </Typography>
+    };
+
+
+    if (!isAdmin && userData && userData.name !== 'unknown') {
+        return <Typography> Hey, {userData.name}! </Typography>;
+    };
     
+    const handlUsernameChange = (event) => {
+        setUsername(event.target.value)
+    };
+
+    const registerNewName = () => {
+        updateUsername(username)
+    };
+
     return (
-        <Typography>
-            {'Please Enter your name'}
-        </Typography>
+        <Box>
+            <TextField
+                label="Enter your name"
+                value={username}
+                onChange={handlUsernameChange}
+            />
+            <Button onClick={registerNewName}>Register</Button>
+        </Box>
     )
 };
 
--- src/functions/hook/useRequest.js
+++ src/functions/hook/useRequest.js
@@ -9,9 +9,10 @@ const useRequest = (requestFunction) => {
         setLoading(true);
         try {
             const response = await requestFunction(...args);
+            setData(response.data)
             return response.data;
         } catch(err) {
-            const errorMessage = err.response && err.response.data && err.response.data.message ? error.data.message : 'Something unexpected happened.';
+            const errorMessage = err.response && err.response.data && err.response.data.message ? error.response.data.message : 'Something unexpected happened.';
             setError({message: errorMessage});
         } finally {
             setLoading(false);
