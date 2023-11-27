0714f1d24153bfe669ef6ff24306d7a5efb485fe
Miles Davis <foldes@mail.com>
2023-11-27 18:31:36 +0100

    prod update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-        <Router basename='/lottokeeper'>
+        <Router>
           <ContextProvider>
             <MainRoutes />
           </ContextProvider>
